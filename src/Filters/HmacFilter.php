31e1d625979dfeecbffbb0c53208e88f64277e56
Sam Mousa <sam@mousa.nl>
2017-11-16 13:00:58 +0100

    Add exception instead of empty response

--- src/Filters/HmacFilter.php
+++ src/Filters/HmacFilter.php
@@ -6,7 +6,9 @@ namespace SamIT\Yii2\Filters;
 
 use SamIT\Yii2\Components\UrlSigner;
 use yii\base\ActionFilter;
+use yii\web\ForbiddenHttpException;
 use yii\web\Request;
+use yii\web\UnauthorizedHttpException;
 
 /**
  * Filter that checks for a valid HMAC in the URL.
@@ -28,7 +30,9 @@ class HmacFilter extends ActionFilter
     {
         /** @var Request $request */
         $request = $action->controller->module->get('request');
-        return $this->signer->verifyHMAC($request->queryParams);
+        if (!$this->signer->verifyHMAC($request->queryParams)) {
+            throw new ForbiddenHttpException("No or invalid HMAC");
+        }
     }
 
 }
